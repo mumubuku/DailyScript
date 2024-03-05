13e004d9e90a97fc28a32c53c153a8aea659b38b
tianyunzhao <tianyunzhao@yanpai.com>
2024-03-05 22:26:56 +0800

    xxx

--- kan/m3u8-kan.py
+++ kan/m3u8-kan.py
@@ -5,15 +5,16 @@ import csv
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 from selenium.webdriver.common.by import By
-from selenium import webdriver
+from selenium.webdriver import Chrome, ChromeOptions
+import sys
 
-def get_subdomains():
+def get_subdomains(arg1,arg2):
 
 
    url = 'https://www.kan9132.com/vodplay/214019-1-1.html'
 
      # 读取文件中的数字
-   if os.path.exists('num.txt'):
+   if os.path.exists(arg1+'num.txt'):
      with open('num.txt', 'r') as f:
       num = int(f.read())
       print(num)
@@ -23,12 +24,17 @@ def get_subdomains():
    
    with open('data.csv', 'a', newline='') as csvfile:
        writer = csv.writer(csvfile)  
-       for number in range(num, 999999):
+       for number in range(arg1, arg2):
         new_url = url.replace('214019', str(number))
         with open('num.txt','w') as f:
          f.write(str(number))
        
-        driver = webdriver.Chrome()
+        chrome_options = ChromeOptions()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        driver = Chrome(options=chrome_options)      
 
         driver.get(new_url)
 
@@ -42,10 +48,17 @@ def get_subdomains():
         endIndex = len(div_html)
         result = div_html[startIndex:endIndex]
         print(result)
-        driver.quit()  
         
+
+        div_element = driver.find_element(By.NAME, 'description')
+        print(div_element)
+        content = div_element.get_attribute('content')
+        print(content)
+        
+        
+        driver.quit()  
         try:
-           writer.writerow([result])
+           writer.writerow([content,result])
            print('CSV文件写入成功')
         except Exception as e:
            print('CSV文件写入失败，错误信息：', e)
@@ -56,4 +69,6 @@ def get_subdomains():
 
 
 url = 'https://www.imooc.com/course/list'
-subdomains = get_subdomains()
+arg1 = sys.argv[1]
+arg2 = sys.argv[2]
+subdomains = get_subdomains(arg1,arg2)
